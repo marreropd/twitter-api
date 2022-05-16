9673deb87fe8ac211591252c410633999e23d38e
marreropd <pablomarrerofernandez1@gmail.com>
2022-05-16 20:46:47 -0300

    update

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
+
 const express = require("express");
+app.use(cors());
 const routes = require("./routes");
 const mongoose = require("mongoose");
 const dbInitialSetup = require("./dbInitialSetup");
 const APP_PORT = process.env.APP_PORT || 8000;
 const app = express();
 var cors = require("cors");
-app.use(cors());
 
 //const MongoStore = require("connect-mongo"); //(session.Store);
 
